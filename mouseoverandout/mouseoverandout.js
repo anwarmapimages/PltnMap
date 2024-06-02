b0fbed035bd981d635ef1010177bf2ebd685c9fa
anwarmapimages <anwarmapimages@gmail.com>
2024-06-03 02:38:36 +0500

    Add files via upload

---
+++ mouseoverandout/mouseoverandout.js
@@ -0,0 +1,239 @@
+	// Add mouseover listener to change fillColor
+Safad_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#32AA32"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+
+// Add mouseout listener to revert fillColor
+Safad_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#32CF32"});
+    this.setOptions({strokeColor: "#32CF32"}); //40% shadow
+});
+
+
+
+Ramla_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#32AA32"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Ramla_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#32CF32"});
+    this.setOptions({strokeColor: "#32CF32"}); //40% shadow
+});
+
+
+
+Nablus_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#32AA32"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Nablus_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#32CF32"});
+    this.setOptions({strokeColor: "#32CF32"}); //40% shadow
+});
+
+
+Haifa_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#32AA32"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Haifa_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#32CF32"});
+    this.setOptions({strokeColor: "#32CF32"}); //40% shadow
+});
+
+
+Bisan_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#32AA32"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Bisan_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#32CF32"});
+    this.setOptions({strokeColor: "#32CF32"}); //40% shadow
+});
+
+
+Beersheba_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#32AA32"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Beersheba_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#32CF32"});
+    this.setOptions({strokeColor: "#32CF32"}); //40% shadow
+});
+
+
+
+Akka_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#EE9900"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Akka_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#EEBB00"});
+    this.setOptions({strokeColor: "#EEBB00"}); //40% shadow
+});
+
+Tulkarm_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#EE9900"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Tulkarm_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#EEBB00"});
+    this.setOptions({strokeColor: "#EEBB00"}); //40% shadow
+});
+
+Jericho_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#EE9900"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Jericho_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#EEBB00"});
+    this.setOptions({strokeColor: "#EEBB00"}); //40% shadow
+});
+
+Hebron_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#EE9900"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Hebron_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#EEBB00"});
+    this.setOptions({strokeColor: "#EEBB00"}); //40% shadow
+});
+
+Lod_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#EE9900"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Lod_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#EEBB00"});
+    this.setOptions({strokeColor: "#EEBB00"}); //40% shadow
+});
+
+
+
+Jenin_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#3377BB"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Jenin_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#3399CC"});
+    this.setOptions({strokeColor: "#3399CC"}); //40% shadow
+});
+
+Salfit_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#3377BB"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Salfit_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#3399CC"});
+    this.setOptions({strokeColor: "#3399CC"}); //40% shadow
+});
+
+Gaza_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#3377BB"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Gaza_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#3399CC"});
+    this.setOptions({strokeColor: "#3399CC"}); //40% shadow
+});
+
+
+
+Alnasra_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#959585"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Alnasra_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#A9A9A9"});
+    this.setOptions({strokeColor: "#A9A9A9"}); //40% shadow
+});
+
+Qalqilya_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#959585"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Qalqilya_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#A9A9A9"});
+    this.setOptions({strokeColor: "#A9A9A9"}); //40% shadow
+});
+
+Jerusalem_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#959585"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Jerusalem_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#A9A9A9"});
+    this.setOptions({strokeColor: "#A9A9A9"}); //40% shadow
+});
+
+
+
+Tiberias_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#BA4000"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Tiberias_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#D84315"});
+    this.setOptions({strokeColor: "#D84315"}); //40% shadow
+});
+
+Tubas_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#BA4000"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Tubas_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#D84315"});
+    this.setOptions({strokeColor: "#D84315"}); //40% shadow
+});
+
+Yaffa_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#BA4000"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Yaffa_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#D84315"});
+    this.setOptions({strokeColor: "#D84315"}); //40% shadow
+});
+
+Ramallah_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#BA4000"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Ramallah_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#D84315"});
+    this.setOptions({strokeColor: "#D84315"}); //40% shadow
+});
+
+
+
+Bethlehem_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#578A3A"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Bethlehem_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#589E3E"});
+    this.setOptions({strokeColor: "#589E3E"}); //40% shadow
+});
+
+
+
+Deadsea_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#1e80EE"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Deadsea_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#1e90ff"});
+    this.setOptions({strokeColor: "#1e90ff"}); //40% shadow
+});
+
+
+
+Bahriatabria_fence.addListener('mouseover', function() {
+    this.setOptions({fillColor: "#1e80EE"}); //40% shadow
+    this.setOptions({strokeColor: "#FFF"}); //40% shadow
+});
+Bahriatabria_fence.addListener('mouseout', function() {
+    this.setOptions({fillColor: "#1e90ff"});
+    this.setOptions({strokeColor: "#1e90ff"}); //40% shadow
+});
